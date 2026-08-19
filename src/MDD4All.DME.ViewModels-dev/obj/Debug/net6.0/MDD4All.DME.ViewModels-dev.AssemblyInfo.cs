345c17bc796d7f051a6f7c551e3ae61966ba16d7
mDuckLab <mmschuessler205@googlemail.com>
2026-08-19 11:29:35 +0200

    presentation: pick data models by namespace, not by assembly

--- src/MDD4All.DME.ViewModels-dev/obj/Debug/net6.0/MDD4All.DME.ViewModels-dev.AssemblyInfo.cs
+++ src/MDD4All.DME.ViewModels-dev/obj/Debug/net6.0/MDD4All.DME.ViewModels-dev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDD4All.DME.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc000f22b67af00f493dbfaa417e79cd21a8b231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564423b36bac1df1cf11732e3ec4c7cb0e27e672")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDD4All.DME.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDD4All.DME.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
